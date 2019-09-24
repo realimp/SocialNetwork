1e24329d4f4df0be2b60b8be810465aef9329b24
X64 <hottaby4hrip@mail.ru>
2019-09-24 16:09:19 +0300

    6-JPA_Person

--- src/test/java/Entitys/Person.java
+++ src/test/java/Entitys/Person.java
@@ -1,7 +1,6 @@
 package Entitys;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,88 +8,43 @@ import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
+@Data
 public class Person {
     @Id
     @GeneratedValue
-    @Getter
-    @Setter
     private Integer id;
 
-    @Getter
-    @Setter
     private String first_name; // имя
 
 
-    @Getter
-    @Setter
     private String last_name; // фамилия
 
 
-    @Getter
-    @Setter
     private Date reg_date; // дата и время регистрации
 
-    @Getter
-    @Setter
     private Date birth_date; // дата рождения
 
+     private String e_mail; //Привязанная электронная почта
 
-    @Getter
-    @Setter
-    private String e_mail; //Привязанная электронная почта
-
-
-    @Getter
-    @Setter
     private String phone; //Привязанный номер телефона
 
-
-    @Getter
-    @Setter
     private String password; //Пароль
 
-
-    @Getter
-    @Setter
     private String photo; //Ссылка на изображение
 
-
-    @Getter
-    @Setter
     private String about; //Текст о себе
 
-
-    @Getter
-    @Setter
     private String city; //Город проживания
 
-    @Getter
-    @Setter
     private String country; //Страна проживания
 
+    private String confirmationCode; //код восстановления пароля / подтверждения регистрации
 
-    @Getter
-    @Setter
-    private String confirmation_code; //код восстановления пароля / подтверждения регистрации
+    private Boolean isApproved; //Подтверждена ли регистрация
 
+    private Boolean messagesPermission; //Разрешение на получение сообщений: ALL(false) - от всех пользователей (кроме заблокированных), FRIENDS(true) - только от друзей
 
-    @Getter
-    @Setter
-    private Boolean is_approved; //Подтверждена ли регистрация
+    private Date lastOnlineTime; //Время последнего пребывания онлайн
 
-
-    @Getter
-    @Setter
-    private Boolean messages_permission; //Разрешение на получение сообщений: ALL(false) - от всех пользователей (кроме заблокированных), FRIENDS(true) - только от друзей
-
-
-    @Getter
-    @Setter
-    private Date last_online_time; //Время последнего пребывания онлайн
-
-
-    @Getter
-    @Setter
-    private Boolean is_blocked; //Блокировка пользователя модератором / администратором
-
+    private Boolean isBlocked; //Блокировка пользователя модератором / администратором
 }
