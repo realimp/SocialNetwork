36e48988a4c5f8483cd1016d1025aeaef37cf50b
NataliaGurevich <gurevich.natalia@gmail.com>
2019-09-27 12:03:55 +0200

    add controller for profile

--- src/main/java/ru/skillbox/socialnetwork/controllers/AccountController.java
+++ src/main/java/ru/skillbox/socialnetwork/controllers/AccountController.java
@@ -1,40 +1,43 @@
 package ru.skillbox.socialnetwork.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/account")
 public class AccountController {
-    @PostMapping("/register")
-    public String register(){
-        return "register";
-    }
 
-    @PutMapping("/password/recovery")
-    public String passwordRecovery(){
-        return "passwordRecovery";
-    }
+  @PostMapping("/register")
+  public String register() {
+    return "register";
+  }
 
-    @PutMapping("/password/set")
-    public String passwordSet(){
-        return "passwordSet";
-    }
-    @PutMapping("/email")
-    public String email(){
-        return "email";
-    }
-    @GetMapping("/notifications")
-    public String notifications(){
-        return "notifications";
-    }
-    @PutMapping("/notifications")
-    public String notifications(Model model){
-        return "notifications";
-    }
+  @PutMapping("/password/recovery")
+  public String passwordRecovery() {
+    return "passwordRecovery";
+  }
 
+  @PutMapping("/password/set")
+  public String passwordSet() {
+    return "passwordSet";
+  }
+
+  @PutMapping("/email")
+  public String email() {
+    return "email";
+  }
+
+  @GetMapping("/notifications")
+  public String notifications() {
+    return "notifications";
+  }
+
+  @PutMapping("/notifications")
+  public String notifications(Model model) {
+    return "notifications";
+  }
 }
---
+++ src/main/java/ru/skillbox/socialnetwork/controllers/ProfileController.java
@@ -0,0 +1,70 @@
+package ru.skillbox.socialnetwork.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class ProfileController {
+
+  //mapping for a current user
+  @GetMapping("/me")
+  public String getMe() {
+    return "get Me";
+  }
+
+  @PutMapping("/me")
+  public String putMe() {
+    return "put Me";
+  }
+
+  @DeleteMapping("/me")
+  public String deleteMe() {
+    return "delete Me";
+  }
+
+  //get user by id
+  @GetMapping("/{id}")
+  public String getUser(@PathVariable int id) {
+    return "get User id = " + id;
+  }
+
+  //getting posts on the user's wall
+  @GetMapping("/{id}/wall")
+  public String getUserWall(@PathVariable int id) {
+    return "User id = " + id + " -> get wall";
+  }
+
+  //adding a post to a user's wall
+  @PostMapping("/{id}/wall")
+  public String postUserWall(@PathVariable int id) {
+    return "User id = " + id + " -> post wall";
+  }
+
+  //user Search
+  @GetMapping("/search")
+  public String getUserSearch(String firstName, String lastName,
+      int ageFrom, int ageTo,
+      Integer countryId, Integer cityId,
+      int offset, int itemPerPage) {
+
+    return "user Search";
+  }
+
+  //block user by id
+  @PutMapping("/block/{id}")
+  public String blockUser(@PathVariable int id) {
+    return "block User id = " + id;
+  }
+
+  //unblock user by id
+  @DeleteMapping("/block/{id}")
+  public String unblockUser(@PathVariable int id) {
+    return "unblock User id = " + id;
+  }
+}
