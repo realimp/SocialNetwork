995a34e6cfa2254cf6d7994b1ad27fc7c23a3c1f
Sergey_Usenko <q1122333@yandex.ru>
2019-10-03 21:47:22 +0700

    moved the test to a folder test/repositories

--- src/test/java/ru/skillbox/socialnetwork/SocialNetworkApplicationTests.java
+++ src/test/java/ru/skillbox/socialnetwork/SocialNetworkApplicationTests.java
@@ -24,41 +24,4 @@ public class SocialNetworkApplicationTests {
     public void sendEmail() {
         eMailService.sendEMail("test@gmail.com", "test@gmail.com", "Test", "Test mail");
     }
-
-    @Autowired
-    private PersonRepository personRepository;
-
-    @Test
-    public void testAddOnePerson() {
-        Person person = new Person();
-        person.setFirstName("Oleg");
-        person.setLastName("Petrov");
-        person.setPhone("9009001010");
-        person.setPassword("password");
-        person.setApproved(true);
-        person.setBlocked(false);
-        person.setOnline(true);
-        person.setDeleted(false);
-        personRepository.save(person);
-    }
-	@Test
-	public void testUpdatePerson() {
-		Person person = new Person();
-		person.setId(1);
-		person.setFirstName("Oleg");
-		person.setLastName("Petrov");
-		person.setPhone("8881112266");
-		person.setPassword("password");
-		person.setApproved(true);
-		person.setBlocked(false);
-		person.setOnline(true);
-		person.setDeleted(false);
-		personRepository.save(person);
-	}
-	@Test
-	public void testDeletePerson() {
-		Person person = new Person();
-		person.setId(1);
-		personRepository.delete(person);
-	}
 }
