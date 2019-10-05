1fab285218810c0c7a50e0fdfb576be67a102cd7
vaselru@gmail.com <vaselru@gmail.com>
2019-10-05 11:14:38 +0300

    https://gitlab.skillbox.ru/JavaPRO2/JavaPRO-SocialNetwork-StudyGroup-2/issues/28 Доработка сущностей респонса

---
+++ src/main/java/ru/skillbox/socialnetwork/api/responses/ResponseList.java
@@ -0,0 +1,37 @@
+package ru.skillbox.socialnetwork.api.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ResponseList<T> extends Response<T>{
+    @JsonProperty private long total;
+    @JsonProperty private long offset;
+    @JsonProperty private long perPage;
+
+    public ResponseList(T data) {
+        super(data);
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    public long getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(long perPage) {
+        this.perPage = perPage;
+    }
+}
