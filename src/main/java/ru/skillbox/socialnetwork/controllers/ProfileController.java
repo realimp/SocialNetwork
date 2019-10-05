02f239d37220fd3a212b5435570580af2fb4ac83
vaselru@gmail.com <vaselru@gmail.com>
2019-10-05 16:16:15 +0300

    https://gitlab.skillbox.ru/JavaPRO2/JavaPRO-SocialNetwork-StudyGroup-2/issues/28 Доработка сущностей респонса

--- src/main/java/ru/skillbox/socialnetwork/controllers/ProfileController.java
+++ src/main/java/ru/skillbox/socialnetwork/controllers/ProfileController.java
@@ -12,7 +12,6 @@ import ru.skillbox.socialnetwork.api.requests.profile.Me;
 import ru.skillbox.socialnetwork.api.requests.profile.Wall;
 import ru.skillbox.socialnetwork.api.responses.*;
 import ru.skillbox.socialnetwork.api.responses.profile.UserDto;
-import ru.skillbox.socialnetwork.entities.Person;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,63 +20,65 @@ import java.util.List;
 @RequestMapping("/users")
 public class ProfileController {
 
-  //mapping for a current user
-  @GetMapping("/me")
-  public Response<PersonDto> getMe() {
-    return new Response<>(new PersonDto());
-  }
+    //mapping for a current user
+    @GetMapping("/me")
+    public Response<PersonDto> getMe() {
+        return new Response<>(new PersonDto());
+    }
 
-  @PutMapping("/me")
-  public Response<PersonDto> putMe(@RequestBody Me me) {
-    return new Response<>(new PersonDto());
-  }
+    @PutMapping("/me")
+    public Response<PersonDto> putMe(@RequestBody Me me) {
+        return new Response<>(new PersonDto());
+    }
 
-  @DeleteMapping("/me")
-  public Response<MessageDto> deleteMe() {
-    return new Response<>(new MessageDto());
-  }
+    @DeleteMapping("/me")
+    public Response<MessageDto> deleteMe() {
+        return new Response<>(new MessageDto());
+    }
 
-  //get user by id
-  @GetMapping("/{id}")
-  public Response<UserDto> getUser(@PathVariable int id) {
-    return new Response<>(new UserDto());
-  }
+    //get user by id
+    @GetMapping("/{id}")
+    public Response<UserDto> getUser(@PathVariable int id) {
+        return new Response<>(new UserDto());
+    }
 
-  //getting posts on the user's wall
-  @GetMapping("/{id}/wall")
-  public ResponseList<List<PersonsWallPostDto>> getUserWall(@PathVariable int id) {
-    List<PersonsWallPostDto> personsWallPostDtoList = new ArrayList<>();
-    personsWallPostDtoList.add(new PersonsWallPostDto());
-    return new ResponseList<>(personsWallPostDtoList);
-  }
+    //getting posts on the user's wall
+    @GetMapping("/{id}/wall")
+    public ResponseList<List<PersonsWallPostDto>> getUserWall(@PathVariable int id) {
+        List<PersonsWallPostDto> personsWallPostDtoList = new ArrayList<>();
+        personsWallPostDtoList.add(new PersonsWallPostDto());
+        return new ResponseList<>(personsWallPostDtoList);
+    }
 
-  //adding a post to a user's wall
-  @PostMapping("/{id}/wall")
-  public Response<PostDto>  postUserWall(@PathVariable int id, @RequestBody Wall wall) {
-    return new Response<>(new PostDto());
-  }
+    //adding a post to a user's wall
+    @PostMapping("/{id}/wall")
+    public Response<PostDto> postUserWall(@PathVariable int id, @RequestBody Wall wall) {
+        return new Response<>(new PostDto());
+    }
 
-  //user Search
-  @GetMapping("/search")
-  public String getUserSearch(String firstName, String lastName,
-      int ageFrom, int ageTo,
-      Integer countryId, Integer cityId,
-      int offset, int itemPerPage) {
+    //user Search
+    @GetMapping("/search")
+    public ResponseList<List<PersonDto>> getUserSearch(String firstName, String lastName,
+                                                       int ageFrom, int ageTo,
+                                                       Integer countryId, Integer cityId,
+                                                       int offset, int itemPerPage) {
 
-    return "user Search";
-  }
+        List<PersonDto> personsDtoList = new ArrayList<>();
+        personsDtoList.add(new PersonDto());
+        return new ResponseList<>(personsDtoList);
+    }
 
-  //block user by id
-  @PutMapping("/block/{id}")
-  public Response<MessageDto>  blockUser(@PathVariable int id) {
-    MessageDto messageDto = new MessageDto();
-    return new Response<>(messageDto);
-  }
+    //block user by id
+    @PutMapping("/block/{id}")
+    public Response<MessageDto> blockUser(@PathVariable int id) {
+        MessageDto messageDto = new MessageDto();
+        return new Response<>(messageDto);
+    }
 
-  //unblock user by id
-  @DeleteMapping("/block/{id}")
-  public Response<MessageDto>  unblockUser(@PathVariable int id) {
-    MessageDto messageDto = new MessageDto();
-    return new Response<>(messageDto);
-  }
+    //unblock user by id
+    @DeleteMapping("/block/{id}")
+    public Response<MessageDto> unblockUser(@PathVariable int id) {
+        MessageDto messageDto = new MessageDto();
+        return new Response<>(messageDto);
+    }
 }
