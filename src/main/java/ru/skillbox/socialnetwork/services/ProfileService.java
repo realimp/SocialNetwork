9cdae3bb7357b2cf64e4979195f0630def408d9a
Savva <savvamail@yandex.ru>
2019-10-07 18:41:38 +0300

    Profile Service was added

--- src/main/java/ru/skillbox/socialnetwork/services/ProfileService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/ProfileService.java
@@ -1,9 +1,12 @@
+/** @Author Savva */
 package ru.skillbox.socialnetwork.services;
 
-import org.hibernate.dialect.Ingres9Dialect;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import ru.skillbox.socialnetwork.entities.Person;
+import ru.skillbox.socialnetwork.entities.Post;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 @Service
@@ -22,6 +25,7 @@ public class ProfileService {
 
     public void editPerson(String first_name, String last_name, Date birth_date, String phone, String photo_id,
                            String about, Integer town_id, Integer country_id, String messages_permission) {
+        //TODO: Доделать setPhoto, setCity, setCountry
         person.setFirstName(first_name);
         person.setLastName(last_name);
         person.setBirthDate(birth_date);
@@ -37,20 +41,32 @@ public class ProfileService {
         person.setDeleted(true);
     }
 
-    public void getPersonById(Integer Id) {
-        /*
-        Откуда берем пользователя?
-         */
+    public Person getPersonById(Integer id) {
+        //TODO: Сделать поиск людей по ID
+        return new Person();
     }
 
-    public void getWallListById(Integer Id, ) {
-        /*
-        Откуда берем записи на стене?
-         */
+    public ArrayList<Post> getWallPostsById(Integer id, Integer offset, Integer itemPerPage) {
+        //TODO: Сделать получение записей на стене по ID пользователя
+        return new ArrayList<>();
     }
 
+    public void addWallPostById(Integer id, Date publish_date) {
+        //TODO: Сделать добавление записи на стену по ID пользователя
+    }
 
+    public ArrayList<Person> searchPerson(String first_name, String last_name, Integer age_from, Integer age_to,
+                                          Integer country_id, Integer city_id, Integer offset, Integer itemPerPage) {
+        //TODO: Сделать поиск пользователей по параметрам
+        return new ArrayList<>();
+    }
 
+    public void blockPersonById(Integer id) {
+        //TODO: Сделать блокировку пользователя по ID
+    }
 
+    public void unblockPersonById(Integer id) {
+        //TODO: Сделать разблокировку пользователя по ID
+    }
 
 }
