966f67dbf1ed8c9420073b073eed786ebeb2b262
Savva <savvamail@yandex.ru>
2019-10-08 16:17:05 +0300

    Patch3

--- src/main/java/ru/skillbox/socialnetwork/services/ProfileService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/ProfileService.java
@@ -1,25 +1,22 @@
 /** @Author Savva */
 package ru.skillbox.socialnetwork.services;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import ru.skillbox.socialnetwork.entities.Person;
 import ru.skillbox.socialnetwork.entities.Post;
 import ru.skillbox.socialnetwork.repositories.PersonRepository;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class ProfileService {
 
     private Person person;
+
+    @Autowired
+    private PersonRepository personRepository;
 
     public ProfileService(Person person) {
         this.person = person;
@@ -31,16 +28,16 @@ public class ProfileService {
     }
 
     public void editPerson(String first_name, String last_name, Date birth_date, String phone, String photo_id,
-                           String about, Integer town_id, Integer country_id, String messages_permission) {
-        //TODO: Доделать setPhoto, setCity, setCountry
+                           String about, String town, String country, String messages_permission) {
+        //TODO: добавить setPhoto.  photo_id ??
         person.setFirstName(first_name);
         person.setLastName(last_name);
         person.setBirthDate(birth_date);
         person.setPhone(phone);
         //person.setPhoto(photo_id);
         person.setAbout(about);
-        //person.setCity(town_id);
-        //person.setCountry(country_id);
+        person.setCity(town);
+        person.setCountry(country);
         person.setMessagesPermission(messages_permission);
     }
 
@@ -48,32 +45,44 @@ public class ProfileService {
         person.setDeleted(true);
     }
 
-    public Person getPersonById(Integer id) {
-        //TODO: Сделать поиск людей по ID
-        return new Person();
+    public Optional<Person> getPersonById(Integer id) {
+        Optional<Person> person = personRepository.findById(id);
+        return person;
     }
 
-    public ArrayList<Post> getWallPostsById(Integer id, Integer offset, Integer itemPerPage) {
-        //TODO: Сделать получение записей на стене по ID пользователя
+    public List<Post> getWallPostsById(Integer id, Integer offset, Integer itemPerPage) {
+        //TODO: Сделать после появления PostRepository
         return new ArrayList<>();
     }
 
     public void addWallPostById(Integer id, Date publish_date) {
-        //TODO: Сделать добавление записи на стену по ID пользователя
+        //TODO: Сделать после появления PostRepository
     }
 
-    public ArrayList<Person> searchPerson(String first_name, String last_name, Integer age_from, Integer age_to,
-                                          Integer country_id, Integer city_id, Integer offset, Integer itemPerPage) {
-        //TODO: Сделать поиск пользователей по параметрам
-        return new ArrayList<>();
+    public Page<Person> searchPerson(String first_name, String last_name, Integer age_from, Integer age_to,
+                                          String country, String city, Integer offset, Integer itemPerPage) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -age_to);
+        Date birthDateFrom = calendar.getTime();
+        calendar.add(Calendar.YEAR, age_to - age_from);
+        Date birthDateTo = calendar.getTime();
+
+        Pageable firstPageWithTenElements = PageRequest.of(0, 10);
+
+        Page<Person> personList = personRepository.findByFirstNameAndLastNameAndCountryAndCityAndBirthDateBetween(
+                first_name, last_name, country, city, birthDateFrom, birthDateTo, firstPageWithTenElements);
+        return  personList;
     }
 
     public void blockPersonById(Integer id) {
-        //TODO: Сделать блокировку пользователя по ID
+        Optional<Person> person = personRepository.findById(id);
+        person.get().setBlocked(true);
     }
 
     public void unblockPersonById(Integer id) {
-        //TODO: Сделать разблокировку пользователя по ID
+        Optional<Person> person = personRepository.findById(id);
+        person.get().setBlocked(false);
     }
 
 }
