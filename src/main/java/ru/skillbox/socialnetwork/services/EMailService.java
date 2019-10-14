dcc561ae6724fc26a2e567a1d4e56016a10abf45
X64 <hottaby4hrip@mail.ru>
2019-10-14 17:21:40 +0300

    22-TestEmail

--- src/main/java/ru/skillbox/socialnetwork/services/EMailService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/EMailService.java
@@ -1,5 +1,8 @@
 package ru.skillbox.socialnetwork.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
@@ -9,11 +12,25 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
+@Service
 public class EMailService {
+    /*
     private String mailServerHostName = "smtp.mail.ru";
     private int mailServerPort = 587;
     private String mailServerUsername = "JavaPro2.SkillBox@mail.ru";
     private String mailServerPassword = "JP2_SkillBox";
+    */
+
+    @Value("${email.host}")
+    private String mailServerHostName;
+    @Value("${email.port}")
+    private int mailServerPort;
+
+    @Value("${email.username}")
+    private String mailServerUsername;
+    @Value("${email.password}")
+    private String mailServerPassword;
+
     private String senderName = mailServerUsername;
     private String senderEmail = mailServerUsername;
 
