7b1408a838b510559170de2bc75f34ba58a8d3a3
FabritsiusDA <fabritsiusda@gmail.com>
2019-10-14 23:16:25 +0500

    implementation change email, password

--- src/main/java/ru/skillbox/socialnetwork/services/AccountService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/AccountService.java
@@ -1,12 +1,12 @@
 package ru.skillbox.socialnetwork.services;
 
-import com.sun.org.apache.xml.internal.utils.SystemIDResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Service;
 import ru.skillbox.socialnetwork.api.responses.Response;
 import ru.skillbox.socialnetwork.api.responses.ResponseMessage;
 import ru.skillbox.socialnetwork.entities.Person;
-import ru.skillbox.socialnetwork.repositories.AccountRepository;
+import ru.skillbox.socialnetwork.repositories.PersonRepository;
 
 import javax.transaction.Transactional;
 
@@ -14,7 +14,9 @@ import javax.transaction.Transactional;
 public class AccountService {
 
     @Autowired
-    AccountRepository accountRepository;
+    private PersonRepository personRepository;
+    private String ABC = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
 
     @Transactional
     public Response<ResponseMessage> register(String email, String passwd1, String passwd2, String firstName, String lastName, String code) {
@@ -30,18 +32,32 @@ public class AccountService {
         person.setEMail(email);
         person.setPassword(passwd1);
         person.setLastName(lastName);
-        accountRepository.saveAndFlush(person);
+        personRepository.saveAndFlush(person);
         ResponseMessage message = new ResponseMessage();
         message.setMessage("ok");
         Response<ResponseMessage> response = new Response<>(message);
         return response;
     }
 
+    @Transactional
     public Response<ResponseMessage> recovery(String email) {
+
+        int count = (int) (Math.random() * 5) + 6;
+        StringBuilder newPas = new StringBuilder();
+
+        for (int i = 0; i < count; i++)
+            newPas.append(ABC.charAt((int) (Math.random() * ABC.length())));
+
+        Person person = getPersonFromSecurity();
+        person.setPassword(newPas.toString());
+
+        personRepository.saveAndFlush(person);
         EMailService eMailService = new EMailService();
-        String link = "link";
-        if (eMailService.sendEMail("JavaPro2.SkillBox@mail.ru", email, "recoveryPassword", link)) {
+        String mailText = "You password has been changed to " + newPas.toString();
+
+        if (eMailService.sendEMail("JavaPro2.SkillBox@mail.ru", email, "recoveryPassword", mailText)) {
             ResponseMessage message = new ResponseMessage();
+            message.setMessage("ok");
             Response<ResponseMessage> response = new Response<>(message);
             return response;
         } else {
@@ -52,8 +68,53 @@ public class AccountService {
         }
     }
 
+    @Transactional
     public Response<ResponseMessage> changePassword(String token, String password) {
-        //token ?
+        Person person = getPersonFromSecurity();
+        person.setPassword(password);
+        person = personRepository.saveAndFlush(person);
+        if (person.getPassword().equals(password)) {
+            ResponseMessage message = new ResponseMessage();
+            message.setMessage("ok");
+            Response<ResponseMessage> response = new Response<>(message);
+            return response;
+        } else {
+            ResponseMessage message = new ResponseMessage();
+            message.setMessage("");
+            Response<ResponseMessage> response = new Response("invalid_request", System.currentTimeMillis(), message);
+            return response;
+        }
+    }
+
+    @Transactional
+    public Response<ResponseMessage> changeEmail(String email) {
+        Person person = getPersonFromSecurity();
+        person.setEMail(email);
+        person = personRepository.saveAndFlush(person);
+        if (person.getEMail().equals(email)) {
+            ResponseMessage message = new ResponseMessage();
+            message.setMessage("ok");
+            Response<ResponseMessage> response = new Response<>(message);
+            return response;
+        } else {
+            ResponseMessage message = new ResponseMessage();
+            message.setMessage("");
+            Response<ResponseMessage> response = new Response("invalid_request", System.currentTimeMillis(), message);
+            return response;
+        }
+    }
+
+    public Response<ResponseMessage> getNotification() {
+        return null;
+    }
+
+    public Response<ResponseMessage> setNotification() {
+        return null;
+    }
+
+
+    private Person getPersonFromSecurity() {
+        //TODO take person from spring security
         return null;
     }
 
