e9c9c1a3a97316c7a0650eaec0cea3fa8799ac29
Sergey_Usenko <q1122333@yandex.ru>
2019-10-24 21:25:10 +0700

    создал маппер класс для Post

--- src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
@@ -2,12 +2,13 @@ package ru.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class SocialNetworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialNetworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkApplication.class, args);
+    }
 
 }
