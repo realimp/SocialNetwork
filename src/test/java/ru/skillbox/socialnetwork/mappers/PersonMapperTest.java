5af043479ef0783aaf833d49473157858d035d08
Natalia Gurevich <gurevich.natalia@gmail.com>
2019-10-25 17:30:06 +0200

    check get-methods

--- src/test/java/ru/skillbox/socialnetwork/mappers/PersonMapperTest.java
+++ src/test/java/ru/skillbox/socialnetwork/mappers/PersonMapperTest.java
@@ -3,8 +3,10 @@ package ru.skillbox.socialnetwork.mappers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+import ru.skillbox.socialnetwork.api.Country;
 import ru.skillbox.socialnetwork.api.responses.PersonResponse;
 import ru.skillbox.socialnetwork.entities.Person;
 
@@ -39,22 +41,52 @@ public class PersonMapperTest {
         person.setBlocked(false);
         PersonResponse personMapper = PersonMapper.getMapping(person);
 
-        String expected = "{id:1," +
-                "first_name:\"Ivanov\"," +
-                "last_name:\"Petr\"," +
-                "reg_date:1559685600000," +
-                "birth_date:633826800000," +
-                "email:\"ivanov@mail.com\"," +
-                "phone:\"1234567890\"," +
-                "photo:\"photo\"," +
-                "about:\"about\"," +
-                "city:{id:1," +
-                "title:\"Moscow\"}," +
-                "country:{id:1," +
-                "title:\"Russia\"}," +
-                "messages_permission:\"FRIENDS\"," +
-                "last_online_time:1571781600000," +
-                "is_blocked:false}";
-        JSONAssert.assertEquals(expected, new ObjectMapper().writeValueAsString(personMapper), true);
+        Integer expectedId = 1;
+        Assert.assertEquals(expectedId, personMapper.getId());
+
+        String expectedFirstName = "Ivanov";
+        Assert.assertEquals(expectedFirstName, personMapper.getFirstName());
+
+        String expectedLastName = "Petr";
+        Assert.assertEquals(expectedLastName, personMapper.getLastName());
+
+        Long expectedRegDate = 1559685600000l;
+        Assert.assertEquals(expectedRegDate, personMapper.getRegDate());
+
+        Long expectedBirthDate = 633826800000l;
+        Assert.assertEquals(expectedBirthDate, personMapper.getBirthDate());
+
+        String expectedEmail = "ivanov@mail.com";
+        Assert.assertEquals(expectedEmail, personMapper.geteMail());
+
+        String expectedPhone = "1234567890";
+        Assert.assertEquals(expectedPhone, personMapper.getPhone());
+
+        String expectedPhoto = "photo";
+        Assert.assertEquals(expectedPhoto, personMapper.getPhoto());
+
+        String expectedAbout = "about";
+        Assert.assertEquals(expectedAbout, personMapper.getAbout());
+
+        Integer expectedCountryId = 1;
+        Assert.assertEquals(expectedCountryId, personMapper.getCountry().getId());
+
+        String expectedCountryTitle = "Russia";
+        Assert.assertEquals(expectedCountryTitle, personMapper.getCountry().getTitle());
+
+        Integer expectedCityId = 1;
+        Assert.assertEquals(expectedCityId, personMapper.getCity().getId());
+
+        String expectedCityTitle = "Moscow";
+        Assert.assertEquals(expectedCityTitle, personMapper.getCity().getTitle());
+
+        String expectedMessagePermission = "FRIENDS";
+        Assert.assertEquals(expectedMessagePermission, personMapper.getMessagesPermission().toString());
+
+        Long expectedLastOnLine = 1571781600000l;
+        Assert.assertEquals(expectedLastOnLine, personMapper.getLastOnlineTime());
+
+        boolean expectedIsBlocked = false;
+        Assert.assertEquals(expectedIsBlocked, personMapper.getBlocked());
     }
 }
