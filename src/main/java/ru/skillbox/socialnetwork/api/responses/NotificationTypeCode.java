3b7209e7cf4997b8554ab91476791b66d239b8c8
Ildar Fatykhov <fat0318@gmail.com>
2019-10-29 18:25:10 +0300

    adding Notification entity and NotificationRepository

--- src/main/java/ru/skillbox/socialnetwork/api/responses/NotificationTypeCode.java
+++ src/main/java/ru/skillbox/socialnetwork/api/responses/NotificationTypeCode.java
@@ -1,9 +1,19 @@
 package ru.skillbox.socialnetwork.api.responses;
 
 public enum NotificationTypeCode {
-    POST,
-    POST_COMMENT,
-    COMMENT_COMMENT,
-    FRIEND_REQUEST,
-    MESSAGE
+    COMMENT_COMMENT(1), FRIEND_REQUEST(2), MESSAGE(3), POST(4), POST_COMMENT(5);
+
+    private Integer code;
+
+    NotificationTypeCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
 }
