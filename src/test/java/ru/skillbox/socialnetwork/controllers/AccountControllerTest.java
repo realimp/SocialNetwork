f90ff69c7f31e88dbc4378d2d2fc828473ea06a0
Natalia Gurevich <gurevich.natalia@gmail.com>
2019-11-08 13:05:11 +0100

    check responses to AccountControllerTest

--- src/test/java/ru/skillbox/socialnetwork/controllers/AccountControllerTest.java
+++ src/test/java/ru/skillbox/socialnetwork/controllers/AccountControllerTest.java
@@ -1,13 +1,18 @@
 package ru.skillbox.socialnetwork.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.skillbox.socialnetwork.api.responses.*;
+import ru.skillbox.socialnetwork.services.AccountService;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -19,39 +24,68 @@ public class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
+    @MockBean
+    private AccountService accountService;
+
+    private MessageResponse message;
+    private Response response;
+
+    @Before
+    public void setUp() {
+        message = new MessageResponse();
+        message.setMessage("ok");
+        long timestamp = 0;
+        response = new Response(message);
+        response.setTimestamp(timestamp);
+    }
+
+        @Test
     public void registerTest() throws Exception{
-        this.mockMvc.perform(post("/account/register")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("register")));
+        this.mockMvc.perform(post("/account/register")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     public void passwordRecoveryTest() throws Exception{
-        this.mockMvc.perform(put("/account/password/recovery")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("passwordRecovery")));
+        this.mockMvc.perform(put("/account/password/recovery")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     public void passwordSetTest() throws Exception{
-        this.mockMvc.perform(put("/account/password/set")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("passwordSet")));
+        this.mockMvc.perform(put("/account/password/set")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     @Test
     public void emailTest() throws Exception{
-        this.mockMvc.perform(put("/account/email")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("email")));
+        this.mockMvc.perform(put("/account/email")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     @Test
     public void notificationsTest() throws Exception {
-        this.mockMvc.perform(get("/account/notifications")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("notifications")));
+        this.mockMvc.perform(get("/account/notifications")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     @Test
     public void putNotificationsTest() throws Exception {
-        this.mockMvc.perform(put("/account/notifications")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("putNotifications")));
+        this.mockMvc.perform(put("/account/notifications")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(response)))
+                .andDo(print()).andExpect(status().isOk());
     }
 }
