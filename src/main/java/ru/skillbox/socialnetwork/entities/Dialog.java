1e3c9c6835b2b4a24cab2c1a0116711d1fba84ed
Nuclear451 <Koryakovsky.Ivan@gmail.com>
2019-11-11 21:21:30 +0500

    temp fix

--- src/main/java/ru/skillbox/socialnetwork/entities/Dialog.java
+++ src/main/java/ru/skillbox/socialnetwork/entities/Dialog.java
@@ -1,70 +1,70 @@
-//Автор:
-//Имя: Дмитрий Хрипков
-//Псевдоним: X64
-//Почта: HDV_1990@mail.ru
-package ru.skillbox.socialnetwork.entities;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "dialog")
-public class Dialog {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(name = "unread_count")
-    private Integer unreadCount;
-
-    @Column(name = "owner_id")
-    private Person ownerId;
-
-    @Column(name = "is_deleted")
-    private boolean isDeleted;
-
-    @Column(name = "invite_code")
-    private String inviteCode ;
-
-    public Dialog() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getUnreadCount() {
-        return unreadCount;
-    }
-
-    public void setUnreadCount(Integer unreadCount) {
-        this.unreadCount = unreadCount;
-    }
-
-    public Person getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(Person ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
-
-    public String getInviteCode() {
-        return inviteCode;
-    }
-
-    public void setInviteCode(String inviteCode) {
-        this.inviteCode = inviteCode;
-    }
-}
+////Автор:
+////Имя: Дмитрий Хрипков
+////Псевдоним: X64
+////Почта: HDV_1990@mail.ru
+//package ru.skillbox.socialnetwork.entities;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "dialog")
+//public class Dialog {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//    @Column(name = "unread_count")
+//    private Integer unreadCount;
+//
+//    @Column(name = "owner_id")
+//    private Integer ownerId;
+//
+//    @Column(name = "is_deleted")
+//    private boolean isDeleted;
+//
+//    @Column(name = "invite_code")
+//    private String inviteCode ;
+//
+//    public Dialog() {
+//    }
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public Integer getUnreadCount() {
+//        return unreadCount;
+//    }
+//
+//    public void setUnreadCount(Integer unreadCount) {
+//        this.unreadCount = unreadCount;
+//    }
+//
+//    public Integer getOwnerId() {
+//        return ownerId;
+//    }
+//
+//    public void setOwnerId(Integer ownerId) {
+//        this.ownerId = ownerId;
+//    }
+//
+//    public boolean isDeleted() {
+//        return isDeleted;
+//    }
+//
+//    public void setDeleted(boolean deleted) {
+//        isDeleted = deleted;
+//    }
+//
+//    public String getInviteCode() {
+//        return inviteCode;
+//    }
+//
+//    public void setInviteCode(String inviteCode) {
+//        this.inviteCode = inviteCode;
+//    }
+//}
