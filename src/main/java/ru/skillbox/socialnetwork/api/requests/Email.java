e49ee2965cb80fc27e94c60b1d000445044867a6
Natalia Gurevich <gurevich.natalia@gmail.com>
2019-11-19 08:18:37 +0100

    Password recovery

---
+++ src/main/java/ru/skillbox/socialnetwork/api/requests/Email.java
@@ -0,0 +1,13 @@
+package ru.skillbox.socialnetwork.api.requests;
+
+public class Email {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
