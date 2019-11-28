d453e0c495f071b486c1d423ba10c800ac17b4c0
Ildar Fatykhov <fat0318@gmail.com>
2019-11-28 15:28:57 +0300

    adding update Online status and LastonLineTime for account after logout

--- src/main/java/ru/skillbox/socialnetwork/controllers/ProfileController.java
+++ src/main/java/ru/skillbox/socialnetwork/controllers/ProfileController.java
@@ -86,11 +86,11 @@ public class ProfileController {
 
     //user Search
     @GetMapping("/search")
-    public ResponseList<List<PersonResponse>> getUserSearch(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName,
+    public ResponseList<List<PersonResponse>> getUserSearch(@RequestParam(required = false) String first_Name, @RequestParam(required = false) String lastName,
                                                             @RequestParam(required = false) Integer ageFrom, @RequestParam(required = false) Integer ageTo,
                                                             @RequestParam(required = false) String country, @RequestParam(required = false) String city,
                                                             @RequestParam(required = false) Integer offset, @RequestParam(required = false) Integer itemPerPage) {
-        String firstNameQuery = firstName != null ? firstName : "";
+        String firstNameQuery = first_Name != null ? first_Name : "";
         String lastNameQuery = lastName != null ? lastName : "";
         int ageSearchFrom = ageFrom != null ? ageFrom : 0;
         int ageSearchTo = ageTo != null ? ageTo : Integer.MAX_VALUE;
