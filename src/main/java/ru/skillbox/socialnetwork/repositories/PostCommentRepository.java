c606e1d00a5dbcdcba666cd3b326765e105e8d44
FabritsiusDA <fabritsiusda@gmail.com>
2019-12-02 23:53:07 +0500

    fix feed service

--- src/main/java/ru/skillbox/socialnetwork/repositories/PostCommentRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/repositories/PostCommentRepository.java
@@ -1,9 +1,17 @@
 package ru.skillbox.socialnetwork.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.skillbox.socialnetwork.entities.PostComment;
 
+import java.util.List;
+
 @Repository
 public interface PostCommentRepository extends JpaRepository<PostComment, Integer> {
+
+    @Query("SELECT p FROM post_comment p WHERE p.post_id =:post_id ")
+    List<PostComment> findByPostId(@Param("post_id") Integer postId);
 }
--- src/main/java/ru/skillbox/socialnetwork/repositories/PostRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/repositories/PostRepository.java
@@ -3,10 +3,14 @@ package ru.skillbox.socialnetwork.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.skillbox.socialnetwork.entities.Person;
 import ru.skillbox.socialnetwork.entities.Post;
+
+import java.util.List;
 
 /*
     @Query("SELECT l FROM CommentLike l JOIN l.comment WHERE l.comment=:comment_id")
@@ -25,4 +29,6 @@ public interface PostRepository extends PagingAndSortingRepository<Post, Integer
 
     Page<Post> findByAuthor(Person author, Pageable pageable);
 
+    @Query(value = "SELECT p FROM post p WHERE p.author_id IN :ids ORDER BY time")
+    Page<Post> findByManyAuthors(@Param("ids") List<Integer> ids);
 }
