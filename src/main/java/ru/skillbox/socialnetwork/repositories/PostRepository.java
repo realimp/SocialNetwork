9d0977c4028058a7c847551cbc56c16ce9a3abcc
FabritsiusDA <fabritsiusda@gmail.com>
2019-12-02 23:53:07 +0500

    fix feed service

--- src/main/java/ru/skillbox/socialnetwork/repositories/PostRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/repositories/PostRepository.java
@@ -31,4 +31,5 @@ public interface PostRepository extends PagingAndSortingRepository<Post, Integer
 
     @Query(value = "SELECT p FROM post p WHERE p.author_id IN :ids ORDER BY time")
     Page<Post> findByManyAuthors(@Param("ids") List<Integer> ids, Pageable pageable);
+
 }
