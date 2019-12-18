e753986a09f62912f2e72fd8bc384f8e68712102
realimp <impinbox@hotmail.com>
2019-12-18 19:21:41 +0300

    ScheduledTasks optimization

--- src/main/java/ru/skillbox/socialnetwork/repositories/NotificationSettingsRepository.java
+++ src/main/java/ru/skillbox/socialnetwork/repositories/NotificationSettingsRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.skillbox.socialnetwork.api.responses.NotificationTypeCode;
 import ru.skillbox.socialnetwork.entities.NotificationSettings;
 import ru.skillbox.socialnetwork.entities.Person;
 
@@ -18,4 +17,7 @@ public interface NotificationSettingsRepository extends JpaRepository<Notificati
 
     @Query("SELECT l FROM NotificationSettings l JOIN l.person WHERE l.person=:person_id AND notification_type_id=:code")
     NotificationSettings findByPersonIdAndSettingCode(@Param("person_id") Person person, Integer code);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM notification_settings l WHERE l.notification_type_id=:type AND l.enable=1")
+    List<NotificationSettings> findByTypeIdAndEnabled(@Param("type") int type);
 }
