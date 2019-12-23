48019c01bdeff2a87a7c49d334ab55db883afc29
realimp <impinbox@hotmail.com>
2019-12-23 23:58:45 +0300

    fixed tags

--- src/main/java/ru/skillbox/socialnetwork/controllers/TagController.java
+++ src/main/java/ru/skillbox/socialnetwork/controllers/TagController.java
@@ -1,13 +1,15 @@
 package ru.skillbox.socialnetwork.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import ru.skillbox.socialnetwork.api.requests.TagRequest;
 import ru.skillbox.socialnetwork.api.responses.*;
-import ru.skillbox.socialnetwork.entities.*;
-import ru.skillbox.socialnetwork.repositories.*;
+import ru.skillbox.socialnetwork.services.TagService;
 
-import java.sql.Timestamp;
 
 @RestController
 @RequestMapping("/tags")
@@ -17,7 +19,7 @@ public class TagController {
   private TagService tagService;
 
   @GetMapping
-  public ResponseList getTags(@RequestParam(required = false) String tag, @RequestParam(required = false) Integer offset, @RequestParam(required = false) Integer itemPerPage) {
+  public ResponseList<List<TagResponse>> getTags(@RequestParam(required = false) String tag, @RequestParam(required = false) Integer offset, @RequestParam(required = false) Integer itemPerPage) {
     int pageOffset = offset != null ? offset : 0;
     int itemsPerPage = itemPerPage != null ? itemPerPage : 20;
     Pageable resultsPage = PageRequest.of(pageOffset, itemsPerPage);
@@ -25,12 +27,12 @@ public class TagController {
   }
 
   @PostMapping
-  public Response postTag(@RequestBody TagRequest tag) {
+  public Response<TagResponse> postTag(@RequestBody TagRequest tag) {
     return tagService.createTag(tag.getTag().trim());
   }
 
   @DeleteMapping
-  public Response tagDelete(@RequestParam int id) {
+  public Response<MessageResponse> tagDelete(@RequestParam int id) {
     return tagService.deleteTag(id);
   }
 }
--- src/main/java/ru/skillbox/socialnetwork/services/TagService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/TagService.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import ru.skillbox.socialnetwork.api.responses.MessageResponse;
+import ru.skillbox.socialnetwork.api.responses.Response;
+import ru.skillbox.socialnetwork.api.responses.ResponseList;
+import ru.skillbox.socialnetwork.api.responses.TagResponse;
 import ru.skillbox.socialnetwork.entities.Tag;
 import ru.skillbox.socialnetwork.repositories.TagRepository;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,31 +23,31 @@ public class TagService {
     @Autowired
     private TagRepository tagRepository;
 
-    public ResponseList getTags(String tag, Pageable pageable) {
+    public ResponseList<List<TagResponse>> getTags(String tag, Pageable pageable) {
         Page<Tag> results = tagRepository.findByTag(tag, pageable);
         if (results.isEmpty()) {
-            return new ResponseList();
+            return new ResponseList<>();
         }
         ArrayList<TagResponse> responseData = new ArrayList<>();
         for (Tag result : results) {
             responseData.add(new TagResponse(result.getId(), result.getText()));
         }
-        return new ResponseList();
+        return new ResponseList<>();
     }
 
-    public Response createTag(String text) {
+    public Response<TagResponse> createTag(String text) {
         if (tagRepository.existsByTag(text.trim())) {
-            return new Response();
+            return new Response<>();
         }
         Tag tag = new Tag();
         tag.setText(text.trim());
         Tag savedTag = tagRepository.saveAndFlush(tag);
-        return new Response(new TagResponse(savedTag.getId(), savedTag.getText()));
+        return new Response<>(new TagResponse(savedTag.getId(), savedTag.getText()));
     }
 
-    public Response deleteTag(int id) {
+    public Response<MessageResponse> deleteTag(int id) {
         tagRepository.delete(tagRepository.findById(id).get());
-        return new Response(new MessageResponse("ok"));
+        return new Response<>(new MessageResponse("ok"));
     }
 
     public Tag saveTag(Tag tag) {
